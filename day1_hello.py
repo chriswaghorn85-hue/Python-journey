5e6936e6e503a6ca7eaa67657a8a1be4cab2ef5a
chriswaghorn85-hue <chriswaghorn85@gmail.com>
2025-11-22 18:28:36 +0000

    Create day1_hello.py

---
+++ day1_hello.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+print(f"Hello {name}! In 5 years you will be {age + 5} and earning money with Python!")
+print("You just ran your first Python program on your phone. Welcome to the future!")
